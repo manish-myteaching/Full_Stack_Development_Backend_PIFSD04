c89a7bc644b2830486b552492e58a3bdc129b142
manish-myteaching <manishverma.myteaching@gmail.com>
2025-04-18 16:48:58 +0530

    Car-engine

---
+++ SpringBoot&Microservices_PI_FSD04/CarEngineApp01/src/startgydesignpattern/DieselEngine.java
@@ -0,0 +1,13 @@
+package startgydesignpattern;
+
+public class DieselEngine implements Engine {
+	
+
+
+	@Override
+	public int start() {
+		System.out.println("Deseal Engine");
+		return 1;
+	}
+
+}
---
+++ SpringBoot&Microservices_PI_FSD04/CarEngineApp01/src/startgydesignpattern/PetrolEngine.java
@@ -0,0 +1,11 @@
+package startgydesignpattern;
+
+public class PetrolEngine implements Engine {
+
+
+	public int start() {
+		System.out.println("Petrol Engine");
+		return 1;
+	}
+
+}
