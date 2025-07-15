d46a7fba5ce5f13dac0eeb9ced075c756295ae3c
manish-myteaching <manishverma.myteaching@gmail.com>
2025-07-15 12:20:03 +0530

    Spring data jpa

---
+++ SpringBoot&Microservices_PI_FSD04/SpringDataJPA/SpringDataJPA-Assiciation-OneToOne01/src/main/java/com/professionalit/SpringDataJpaAssiciationOneToOneApplication.java
@@ -0,0 +1,14 @@
+package com.professionalit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaAssiciationOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaAssiciationOneToOneApplication.class, args);
+
+	}
+
+}
